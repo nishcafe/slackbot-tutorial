d9c97d995142940e741920e4990fb4360884534b
nishcafe <46063774+nishcafe@users.noreply.github.com>
2019-05-31 14:42:33 +0700

    Slackbot tutorial

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, make_response, Response
 import json
-from tokens import SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN
+# from token import SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN
 from slackclient import SlackClient
 from slashCommand import *
 
 
+SLACK_BOT_TOKEN="xoxb-635502118290-647467074757-zPU3GKYLsQPpnV3NROYPMI46"
+SLACK_VERIFICATION_TOKEN="xoxp-635502118290-649096047702-649327190535-1cd0ab1dafac0d3419c527ee4426498e"
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 app = Flask(__name__)
@@ -39,7 +41,7 @@ def command():
   channelMsg = slack_client.api_call(
     "chat.postMessage",
     channel="#" + info["channel_name"],
-    text=commander.getMessage()
+    text=commander.getMessage())
 
   return make_response("", 200)
 
